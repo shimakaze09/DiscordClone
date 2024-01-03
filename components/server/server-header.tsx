e69a8aeefa2292b216e56a872d573cc056c4ceab
John <yangtianzhuo970425@gmail.com>
2024-01-03 18:11:29 +1300

    Server Settings

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -53,7 +53,7 @@ export const ServerHeader = ({
             >
                 {isModerator && (
                     <DropdownMenuItem
-                    onClick={()=>onOpen("invite",{server})}
+                        onClick={() => onOpen("invite", { server })}
                         className="text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer"
                     >
                         Invite People
@@ -62,6 +62,7 @@ export const ServerHeader = ({
                 )}
                 {isAdmin && (
                     <DropdownMenuItem
+                        onClick={() => onOpen("editServer", { server })}
                         className="px-3 py-2 text-sm cursor-pointer"
                     >
                         Server Settings
